ff8f918e571022584dfd87769b067fb29a2d9d76
Oleg Zagryazkin <zagryazkinoleg@yandex.ru>
2022-12-04 19:11:04 +0300

    1-st review 1

---
+++ README.md
@@ -0,0 +1,77 @@
+# API_YAMDB
+REST API проект для сервиса YaMDb — сбор отзывов о фильмах, книгах или музыке.
+
+## Описание
+
+Проект YaMDb собирает отзывы пользователей на произведения.
+Произведения делятся на категории: «Книги», «Фильмы», «Музыка».
+Список категорий  может быть расширен (например, можно добавить категорию «Изобразительное искусство» или «Ювелирка»).
+### Как запустить проект:
+
+Все описанное ниже относится к ОС Linux.
+Клонируем репозиторий и переходим в директорию api_yamdb :
+```bash
+git clone https://github.com/themasterid/infra_sp2
+cd infra_sp2/api_yamdb
+```
+
+Создаем и активируем виртуальное окружение:
+```bash
+python3 -m venv venv
+source /venv/Scripts/activate 
+```
+Ставим модули из requirements.txt:
+```bash
+pip install -r requirements.txt
+```
+
+Переходим в папку с файлом docker-compose.yaml:
+```bash
+cd infra
+```
+
+Поднимаем контейнеры (db-1, web-1, nginx-1):
+```bash
+docker-compose up -d --build
+```
+
+Выполняем миграции:
+```bash
+docker-compose exec web python manage.py makemigrations reviews
+```
+```bash
+docker-compose exec web python manage.py migrate
+```
+
+Создаем суперпользователя:
+```bash
+docker-compose exec web python manage.py createsuperuser
+```
+
+Србираем статику:
+```bash
+docker-compose exec web python manage.py collectstatic --no-input
+```
+
+Создаем дамп базы данных:
+```bash
+docker-compose exec web python manage.py dumpdata > dumpPostrgeSQL.json
+```
+
+Останавливаем контейнеры:
+```bash
+docker-compose down -v
+```
+
+### Шаблон наполнения .env (не включен в текущий репозиторий) расположенный по пути infra/.env
+```
+DB_ENGINE=django.db.backends.postgresql
+DB_NAME=postgres
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+DB_HOST=db
+DB_PORT=5432
+```
+
+### Документация API YaMDb
+Документация доступна по эндпойнту: http://localhost/redoc/
---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,87 @@
+from rest_framework import serializers
+from reviews.models import Title, Genre, Category, Review, Comments
+from rest_framework.relations import SlugRelatedField
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
+from django.db.models import Avg, F
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        exclude = ['id']
+        model = Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        exclude = ['id']
+        model = Category
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True,
+    )
+    author = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
+        slug_field='username',
+        read_only=True
+    )
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if request.method == 'POST':
+            if Review.objects.filter(title=title, author=author).exists():
+                raise ValidationError('вы уже оставили отзыв')
+        return data
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Comments
+        fields = '__all__'
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        slug_field='slug', many=True, queryset=Genre.objects.all()
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Category.objects.all()
+    )
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+
+class ReadOnlyTitleSerializer(serializers.ModelSerializer):
+    rating = serializers.IntegerField(
+        source='reviews__score__avg', read_only=True
+    )
+    genre = GenreSerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
